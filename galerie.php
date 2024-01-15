dd0ffd4dee71be37e3eb1fce2e9f1e82dfa42814
Movva <81530181+movva-gpu@users.noreply.github.com>
2024-01-15 11:39:39 +0100

    Initial Commit

---
+++ galerie.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>R.T.D. Davis</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/styles.css'>
+    <script src='js/main.js'></script>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href='index.html'>Accueil</a></li>
+                <li><a href='about.html'>A propos</a></li>
+                <li><a href='contact.html'>Contact</a></li>
+            </ul>
+        </nav>
+        <h3>R.T.D. Davis</h3>
+    </header>
+    <main>
+        <h1>Galerie</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero veniam exercitationem vitae provident delectus sunt, ea velit molestiae animi veritatis reiciendis assumenda tempora suscipit. Repellat voluptatibus aut qui et asperiores.
+        Vitae, asperiores totam aliquid ut aliquam consequatur necessitatibus molestias blanditiis officiis quos illum laudantium perferendis exercitationem, et quidem harum sed? Expedita at ipsum nostrum ipsa maiores. Architecto fuga ullam repellat.
+        Eligendi adipisci sapiente cupiditate aliquid tempora! Eligendi ea nesciunt quam ad tempora consequuntur soluta excepturi vitae et. Sapiente officiis pariatur, molestiae voluptas iure magnam fugit eligendi? Illum quisquam numquam saepe.
+        Similique deserunt dicta ratione corrupti omnis, ea amet libero nemo quas, necessitatibus odit nulla esse, exercitationem quisquam cumque fugiat dolorem ducimus distinctio laudantium. Labore nam doloremque natus distinctio cumque quae.
+        Sit nostrum voluptas maxime officiis dicta nesciunt nihil ratione suscipit dolor veritatis reiciendis aperiam dolorem quas explicabo, molestias, officia ipsam totam temporibus harum placeat amet consectetur! Velit debitis perferendis ab?
+        Temporibus, doloribus. Facilis ut, quas quod praesentium deserunt a assumenda, excepturi mollitia recusandae corporis autem quo numquam quae rerum sed ad ab labore tempora, laudantium molestias! Quia excepturi placeat sit?
+        Sequi ratione impedit asperiores maiores, tenetur non totam et harum soluta quaerat quia similique quis quo iusto. At animi repellendus aliquam cupiditate provident. Tempora ad unde, labore facilis corporis molestiae.</p>
+    </main>
+    <footer>
+        <ul>
+
+        </ul>
+    </footer>
+</body>
+</html>
