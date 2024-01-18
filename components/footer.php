68037ef29a00df6a3268b38abf27add3feceebce
Movva <81530181+movva-gpu@users.noreply.github.com>
2024-01-18 14:49:16 +0100

    *

--- components/footer.php
+++ components/footer.php
@@ -2,9 +2,6 @@
     <p>
         Sous license <a rel='nofollow' href='LICENSE' title='La license du projet'>MIT</a> • Site créé par <a rel='me nofollow' href='https://piaille.fr/@danyella_strikann' title='Mon profil Mastodon'>Danyella Strikann</a> •
         <a href='CREDITS'>Crédits</a> •
-        <a href='https://github.com/movva-gpu/SAE105'>Source</a> •
-        <a href="https://www.gueury.com/mozilla/">
-            <img src="https://www.gueury.com/mozilla/tidy_16.gif" alt="Validated by HTML Validator (based on Tidy) " height="16" width="39" />
-        </a>
+        <a href='https://github.com/movva-gpu/SAE105'>Source</a>
     </p>
 </footer>
