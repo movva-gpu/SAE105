bb3143518d97f8566af54d0345aa9e64f8eb0452
Movva <81530181+movva-gpu@users.noreply.github.com>
2024-01-18 08:17:46 +0100

    Gallerie.php partially done sooner than asked.

--- galerie.php
+++ galerie.php
@@ -17,13 +17,18 @@
     <main>
         <article>
             <h1>Galerie</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero veniam exercitationem vitae provident delectus sunt, ea velit molestiae animi veritatis reiciendis assumenda tempora suscipit. Repellat voluptatibus aut qui et asperiores.
-                Vitae, asperiores totam aliquid ut aliquam consequatur necessitatibus molestias blanditiis officiis quos illum laudantium perferendis exercitationem, et quidem harum sed? Expedita at ipsum nostrum ipsa maiores. Architecto fuga ullam repellat.
-                Eligendi adipisci sapiente cupiditate aliquid tempora! Eligendi ea nesciunt quam ad tempora consequuntur soluta excepturi vitae et. Sapiente officiis pariatur, molestiae voluptas iure magnam fugit eligendi? Illum quisquam numquam saepe.
-                Similique deserunt dicta ratione corrupti omnis, ea amet libero nemo quas, necessitatibus odit nulla esse, exercitationem quisquam cumque fugiat dolorem ducimus distinctio laudantium. Labore nam doloremque natus distinctio cumque quae.
-                Sit nostrum voluptas maxime officiis dicta nesciunt nihil ratione suscipit dolor veritatis reiciendis aperiam dolorem quas explicabo, molestias, officia ipsam totam temporibus harum placeat amet consectetur! Velit debitis perferendis ab?
-                Temporibus, doloribus. Facilis ut, quas quod praesentium deserunt a assumenda, excepturi mollitia recusandae corporis autem quo numquam quae rerum sed ad ab labore tempora, laudantium molestias! Quia excepturi placeat sit?
-                Sequi ratione impedit asperiores maiores, tenetur non totam et harum soluta quaerat quia similique quis quo iusto. At animi repellendus aliquam cupiditate provident. Tempora ad unde, labore facilis corporis molestiae.</p>
+            <div id='gallery'>
+            <?php
+                $gallery_path = 'images/galerie/';
+                $gallery_folder = opendir($gallery_path);
+                while (($file = readdir($gallery_folder))!== false) {
+                    if ($file == '.' || $file == '..' || $file == 'alt') continue;
+                    echo '<img src='.$gallery_path.$file.
+                    ' alt="'.file_get_contents($gallery_path.'alt/'.$file.'.txt').
+                    '" title="'.file_get_contents($gallery_path.'alt/'.$file.'.txt').'">';
+                }
+            ?>
+            </div>
         </article>
     </main>
     <?php require_once('components/footer.php');?>
