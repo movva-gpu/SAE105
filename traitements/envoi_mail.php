59275e6effaef6ecc789cbe26fc65d8eaa26c1b8
Danyella-linux <81530181+movva-gpu@users.noreply.github.com>
2024-01-24 11:30:15 +0100

    TD09 start

--- traitements/envoi_mail.php
+++ traitements/envoi_mail.php
@@ -1,12 +1,11 @@
 <?php
 
-if (count($_POST) == 0) { header('location: ../contact.php?error=empty'); die; }
-if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) { header('location:../contact.php?error=email'); die; }
+$errors = [];
 
-if (empty($_POST['name'])) { header('location:../contact.php?error=empty-name'); die; }
-if (empty($_POST['first-name'])) { header('location:../contact.php?error=empty-first-name'); die; }
-if (empty($_POST['email'])) { header('location:../contact.php?error=empty-email'); die; }
-if (empty($_POST['message'])) { header('location:../contact.php?error=empty-message'); die; }
+if (count($_POST) == 0) array_push($errors, 'empty');
+if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) array_push($errors, 'email');
+
+if (empty($_POST['name'])) array_push($errors, 'empty');
 
 $name = mb_strtolower($_POST['name']);
 $firstName = mb_strtolower($_POST['first-name']);
@@ -14,7 +13,7 @@ $firstName = mb_strtolower($_POST['first-name']);
 $email = $_POST['email'];
 $subject = $_POST['subject'] ? mb_strtolower($_POST['subject']) : 'Pas d\'objet';
 
-$subject = $subject . ' : de la part de ' . $firstName . ' '. $name;
+$subject = $subject . ' : de la part de ' . ucfirst($firstName) . ' '. ucfirst($name);
 
 $message = $_POST['message'];
 
@@ -26,4 +25,4 @@ if (!mail($dest, $subject, $message, $headers)) { header('location: ../contact.p
 
 $headers = 'From:' . $dest . "\r\n" . 'Reply-to:' . 'noreply@mmi-troyes.fr' . "\r\n" .  'X-Mailer:PHP/' . phpversion();
 
-if (mail($email, 'Confirmation', $firstName . ",\r\n" . 'votre demande de contact auprès de New Who a été enregistré.', $headers)) { header('location: ../contact.php?error=none'); die; }
+if (mail($email, 'Confirmation', ucfirst($firstName) . ",\r\n" . 'votre demande de contact auprès de New Who a été enregistré.', $headers)) { header('location: ../contact.php?error=none'); die; }
