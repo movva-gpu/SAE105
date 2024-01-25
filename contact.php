f7b6eea119c15dd7468b2b24ff940a4fdd44f10b
Movva <81530181+movva-gpu@users.noreply.github.com>
2024-01-25 10:59:56 +0100

    YES DONE

--- contact.php
+++ contact.php
@@ -19,7 +19,6 @@ error_reporting(0);
             if (!empty($_GET['errors'])) {
                 $errors = explode(',', $_GET['errors']);
                 if ($_GET['errors'] != 'none') {
-                    echo count($errors) > 1 ? 'azertyuiop' : 'qsdfghjklm';
                     if (count($errors) > 1) $errors = array_slice($errors, 0, count($errors) - 1);
                     $new_array = [];
                     foreach ($errors as $error) {
