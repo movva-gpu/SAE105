d0ea371d268079d190e54e50bad400469a89c3de
Movva <81530181+movva-gpu@users.noreply.github.com>
2024-01-25 10:41:43 +0100

    I'm dumb

--- traitements/envoi_mail.php
+++ traitements/envoi_mail.php
@@ -7,7 +7,7 @@ if (count($_POST) == 0) array_push($errors, 'empty');
 echo "Checking for valid email address...";
 if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) array_push($errors, 'email');
 
-if (explode('@', $_POST['email']) < 2 || explode('.', explode('@', $_POST['email'])[1]) < 2) array_push($errors, 'email');
+if (count(explode('@', $_POST['email'])) < 2 || count(explode('.', explode('@', $_POST['email'])[1])) < 2) array_push($errors, 'email');
 
 echo "Checking for empty name...";
 if (empty($_POST['name'])) array_push($errors, 'empty');
