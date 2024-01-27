e50f89aa7909c4ac667ae88fdd99eef2d3ae1109
Movva <81530181+movva-gpu@users.noreply.github.com>
2024-01-27 19:49:44 +0100

    added mkv format as AV1

--- index.php
+++ index.php
@@ -30,7 +30,8 @@ if (isset($_POST['watch-again'])) $watch_again = true; ?>
                     <path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z" />
                 </svg>
             </button>
-            <video id="video" preload="auto" muted src="assets/dw_logo_intro.mp4" type="video/mp4">
+            <video id="video" preload="auto" muted>
+                <source src="assets/dw_logo_intro.mkv" type="video/mkv">
                 <source src="assets/dw_logo_intro.mp4" type="video/mp4">
             </video>';
         $_SESSION['video_watched'] = true;
