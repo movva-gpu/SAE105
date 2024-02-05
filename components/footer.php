66311fc6ddc2b0d694cf300fbb618419db7a89c5
Movva <81530181+movva-gpu@users.noreply.github.com>
2024-02-05 12:51:10 +0100

    feat:Optimizing use of DataTables

--- components/footer.php
+++ components/footer.php
@@ -4,11 +4,11 @@ error_reporting(0)
 <footer>
     <p>
         Sous license <a rel='nofollow' href='LICENSE' title='La license du projet'>
-            <img height="12" alt="logo de Creative Commons" src="https://cdn.simpleicons.org/creativecommons/555">
+            <img height="12" width="12" alt="logo de Creative Commons" src="https://cdn.simpleicons.org/creativecommons/555">
             MIT
         </a> • 
         Site créé par <a rel='me nofollow' href='https://piaille.fr/@danyella_strikann' title='Mon profil Mastodon'>
-            <img height="12" alt="Logo de Mastodon" src="https://cdn.simpleicons.org/mastodon/555">
+            <img height="12" width="12" alt="Logo de Mastodon" src="https://cdn.simpleicons.org/mastodon/555">
             Danyella Strikann
         </a> • 
         <?php
@@ -17,7 +17,7 @@ error_reporting(0)
         }
         ?>
         <a href='https://github.com/movva-gpu/SAE105' title="Source du projet sur GitHub">
-            <img height="12" title="Logo de Github" src="https://cdn.simpleicons.org/github/555">
+            <img height="12" width="12" title="Logo de Github" src="https://cdn.simpleicons.org/github/555">
             Source
         </a>
     </p>
@@ -25,8 +25,11 @@ error_reporting(0)
 
 <script src='https://code.jquery.com/jquery-3.7.0.min.js'></script>
 
-<script src='https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js'></script>
-<script src='https://cdn.datatables.net/plug-ins/1.13.7/sorting/date-euro.js'></script>
-<script src='https://cdn.datatables.net/plug-ins/1.13.7/sorting/any-number.min.js'></script>
+<?php
+if (str_contains($_SERVER['PHP_SELF'], 'episodes.php')) echo '
+    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>' . "\n" .
+    '<script src="https://cdn.datatables.net/plug-ins/1.13.7/sorting/date-euro.js"></script>' . "\n" .
+    '<script src="https://cdn.datatables.net/plug-ins/1.13.7/sorting/any-number.min.js"></script>'
+?>
 
 <script src='js/main.min.js' integrity="sha256-fcUbOOiPFxKgQ4b8+l62tX3ehloPgnkJbMfKnKLq6zQ= sha384-QFFdz+so7kf9tH3MhR6B3kB53597wNbGyn/ir9XCV/lXaw0d52KgGA5lvUNoTfDL sha512-T0K8J57bItY+gNDWl52aPsDxeetGN8YSbAudnS3iSQmvy3AVl09R04iBnE3qqZfqKWCfUD2O+BC2EB/aX50jGQ==" crossorigin="anonymous"></script>
